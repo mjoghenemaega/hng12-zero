df7755ef3a87e3a8f063e9ccdd9fc7380dc9d3b1
mjoghenemaega <mjoghenemaega@gmail.com>
2025-01-31 23:52:42 +0100

    final commit

--- api/views.py
+++ api/views.py
@@ -6,8 +6,8 @@ import pytz
 @api_view(["GET"])
 def get_info(request):
     data = {
-        "email": "your-email@example.com",
+        "email": "mjoghenemaega@gmail.com",
         "current_datetime": datetime.now(pytz.utc).isoformat(),
-        "github_url": "https://github.com/yourusername/your-repo"
+        "github_url": "https://github.com/mjoghenemaega/hng12-zero"
     }
     return Response(data)
